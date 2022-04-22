97b3863d648e7a161210890ab613e9ffc5fdea05
k1zn <46452865+k1zn@users.noreply.github.com>
2022-04-22 21:08:52 +0500

    v1.0.0

---
+++ index.js
@@ -0,0 +1,23 @@
+import fetch from 'node-fetch';
+
+class Alga {
+  constructor(number) {
+    if(!number) throw new Error('Card number is required.');
+    this.number = number?.split ? number.split(' ').join('') : number;
+    console.log(this.number)
+  }
+
+  async getBalance() {
+    let req = await fetch('https://pay.brsc.ru/Alga.pay/BalanceOnTheCard.php', {
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      method: 'POST',
+      body: `cardnumber=${this.number}`,
+    })
+    let text = await req.text();
+
+    return (/value=(.+) id="balance"/).exec(text)?.[1] || "-1"
+  }
+
+}
